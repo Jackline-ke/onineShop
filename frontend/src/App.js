19d9fc76b5b2a978449e06bfea5e666681f82aff
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-24 14:24:16 +0300

    files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,10 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Blog from './pages/Blog';
-import Cart from './pages/Cart';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import { Navbar } from './components/Navbar';
+import Main from './Main'
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <Navbar/> 
-        <Routes>
-          <Route path='/' element={<Home/>} />
-          <Route path='blog' element={<Blog/>} />
-          <Route path='contact' element={<Contact/>} />
-          <Route path='cart' element={<Cart/>} />
-        </Routes>
-      </Router>
+      <Main/>
     </div>
   )
 }
---
+++ frontend/src/Main.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Blog from './pages/Blog';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import { Navbar } from './components/Navbar';
+
+const Main = () => {
+  return (
+    <div>
+      <Router>
+        <Navbar/> 
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='blog' element={<Blog/>} />
+          <Route path='contact' element={<Contact/>} />
+          <Route path='cart' element={<Cart/>} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default Main
