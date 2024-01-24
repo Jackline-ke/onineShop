73cdb3568e4535131be7adc418ebc6fd4833de14
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-24 14:55:03 +0300

    files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react'
-import Main from './Main'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Blog from './pages/Blog';
+import Cart from './pages/Cart';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import { Navbar } from './components/Navbar';
 
 const App = () => {
   return (
     <div>
-      <Main/>
+      <Router>
+        <Navbar/> 
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='blog' element={<Blog/>} />
+          <Route path='contact' element={<Contact/>} />
+          <Route path='cart' element={<Cart/>} />
+        </Routes>
+      </Router>
     </div>
   )
 }
