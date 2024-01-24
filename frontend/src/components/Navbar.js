05cf8fb2313bb3d169fe116714df590f5288302e
Jackline-ke <antoninahodhiambo@gmail.com>
2024-01-24 09:59:43 +0300

    files

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,50 @@
+import React, {useState} from 'react'
+import {FaBars} from 'react-icons/fa'
+import {Link} from 'react-scroll'
+
+export const Navbar = () => {
+    const[nav, setNav] = useState(false)
+    const handleNav = ()=>{
+      setNav(!nav)
+    }
+    const closeNav = () => {
+        setNav(false);
+    } 
+  return (
+    <div className='fixed w-full px-8 py-4 md:px-16 text-[#4d3319] font-serif bg-opacity-70 shadow-md' style={{ backdropFilter: 'blur(5px)', webkitBackdropFilter: 'blur(5px)'/* Set a higher z-index */ }}>
+        <div className='flex justify-between items-center'>
+            <h1>Qlynn</h1>
+            <div className='hidden lg:flex gap-8 cursor-pointer'>
+                <Link to='home' smooth={true} duration={2000}>
+                    <h2>Home</h2>
+                </Link>
+                <Link to='blog' smooth={true} duration={2000}>
+                    <h2>Blogs</h2>
+                </Link>
+                <Link to='contact' smooth={true} duration={2000}>
+                    <h2>Contact us</h2>
+                </Link>
+            </div>
+            {/* humburger */}
+            <div onClick={handleNav} className='lg:hidden z-10'>
+                <FaBars size={20} className='cursor-pointer'/>
+            </div>
+            {/* mobile */}
+            <div onClick={handleNav} className={nav ? 'overflow-hidden lg:hidden ease-in duration-300 absolute text-[#f9f2ec] top-0 right-0  w-[40%] bg-[#4d3319]/90 px-4 py-16' : 'absolute left-[-100%] top-0 h-screen ease-in duration-500' }>
+                <div  className='flex flex-col gap-4 cursor-pointer text-lg'>
+                <Link to='home' smooth={true} duration={2000} onClick={closeNav}>
+                    <h2>Home</h2>
+                </Link>
+                <Link to='blog' smooth={true} duration={2000} onClick={closeNav}>
+                    <h2>Blogs</h2>
+                </Link>
+                <Link to='contact' smooth={true} duration={2000} onClick={closeNav}>
+                    <h2>Contact us</h2>
+                </Link>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+  )
+}
---
+++ frontend/src/pages/Blog.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+    <div id='blog' className='w-full h-screen flex items-center justify-center'>Blog</div>
+  )
+}
+
+export default Blog
